c444e6c8c6affe60e5bd26a3aa5c34737a187a04
Waterpicker <Waterpicker@rocketmail.com>
2015-12-27 11:42:59 -0600

    Configuration Works

--- src/main/java/org/waterpicker/paydirtwashingplant/Config.java
+++ src/main/java/org/waterpicker/paydirtwashingplant/Config.java
@@ -1,8 +1,7 @@
 package org.waterpicker.paydirtwashingplant;
 import net.minecraftforge.common.config.Configuration;
 
-import java.io.File;
-import java.util.List;
+import java.io.File;;
 
 /**
  * Created by Waterpician on 12/26/2015.
@@ -18,9 +17,14 @@ public class Config {
 
     public static void setup(File file) {
         Configuration config = new Configuration(file);
+        config.load();
         BLOCK_VOLTAGE = config.get(Configuration.CATEGORY_GENERAL, "voltage", "low").getString();
         BLOCK_EU_BUFFER = config.get(Configuration.CATEGORY_GENERAL, "buffer", 1000).getInt();
         NEEDED_EU_PER_OPERATION = config.get(Configuration.CATEGORY_GENERAL, "eu per operation", 50).getInt();
         WATER_NEEDED_PER_OPERATION = config.get(Configuration.CATEGORY_GENERAL, "water (mb) per operation", 50).getInt();
+        COBBLESTONE_PERCENTAGE = config.get(Configuration.CATEGORY_GENERAL, "cobblestone percentage", 0.05d).getDouble();
+        GRAVEL_PERCENTAGE = config.get(Configuration.CATEGORY_GENERAL, "gravel percentage", 0.05d).getDouble();
+        DIRT_PERCENTAGE = config.get(Configuration.CATEGORY_GENERAL, "dirt percentage", 0.05d).getDouble();
+        config.save();
     }
 }
